ccdcf06b1963c1f647bde085d1fdd651bb4144a4
Eric Puchmayr <epuchmayr@gmail.com>
2023-07-05 13:51:21 -0700

    add hook for resize and browser chrome changes

--- components/App.tsx
+++ components/App.tsx
@@ -10,11 +10,20 @@ import PreviewText from "./PreviewText";
 
 export default function App() {
 
+  // hook to update '--vh' value when window is resized or browser chrome changes
   useEffect(() => {
-    // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
-    let vh = window.innerHeight * 0.01;
-    // Then we set the value in the --vh custom property to the root of the document
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
+    let setViewHeight = () => {
+      // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+      let vh = window.innerHeight * 0.01;
+      // Then we set the value in the --vh custom property to the root of the document
+      document.documentElement.style.setProperty('--vh', `${vh}px`);
+    }
+
+    window.addEventListener('resize', setViewHeight)
+
+    return () => {
+      window.removeEventListener('resize', setViewHeight)
+    }
   },[])
 
   const [currentView, setCurrentView] = useState(VIEWS.SELECT);
